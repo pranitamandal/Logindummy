8c980b2d29b72487dd006dfa258286aedfe89120
Pranita mandal <pranitamandal823@gmail.com>
2024-08-01 12:35:01 +0530

    2nd commit

--- GitProjectFramework/src/test/java/pac1/ContactTest.java
+++ GitProjectFramework/src/test/java/pac1/ContactTest.java
@@ -21,8 +21,5 @@ public void createContactTest() {
 public void modifyContactTest() {
 	System.out.println("execute modifyContactTest ");
 }
-@Test
-public void deleteContactTest() {
-	System.out.println("execute deleteContactTest ");
-}
+
 }
